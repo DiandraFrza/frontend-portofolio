ab7a6a18e1e43232731ac6aeccdd97d07b51f397
DiandrFrzaa_ <diandranasywan@gmail.com>
2025-04-30 09:44:16 +0700

    Frontend Animasi

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,25 +6,34 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('hidden');
 });
 
-// Toggle Dark Mode
-const darkToggle = document.getElementById('dark-toggle');
-const html = document.documentElement;
 
-darkToggle.addEventListener('change', () => {
-  if (darkToggle.checked) {
-    html.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    html.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
-  }
-});
+
+  AOS.init({
+    duration: 1000, // durasi animasi dalam ms
+    once: true // animasi hanya terjadi sekali
+  });
+
+
+
+// Toggle Dark Mode
+// const darkToggle = document.getElementById('dark-toggle');
+// const html = document.documentElement;
+
+// darkToggle.addEventListener('change', () => {
+//   if (darkToggle.checked) {
+//     html.classList.add('dark');
+//     localStorage.setItem('theme', 'dark');
+//   } else {
+//     html.classList.remove('dark');
+//     localStorage.setItem('theme', 'light');
+//   }
+// });
 
 // On load: check localStorage
-window.addEventListener('DOMContentLoaded', () => {
-  const theme = localStorage.getItem('theme');
-  if (theme === 'dark') {
-    html.classList.add('dark');
-    darkToggle.checked = true;
-  }
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//   const theme = localStorage.getItem('theme');
+//   if (theme === 'dark') {
+//     html.classList.add('dark');
+//     darkToggle.checked = true;
+//   }
+// });
